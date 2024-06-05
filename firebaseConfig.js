3efb7dfbd87d18515514faa3f8b5f5dc99cd3f84
ROBERT <jacakrobert33@gmail.com>
2024-06-05 12:41:23 +0200

    gitignore

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { getApp, initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
@@ -11,25 +11,34 @@ import {
   connectFunctionsEmulator,
   httpsCallable,
 } from "firebase/functions";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "@env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDPnxi4sLB9LqFIntoFVH0Dzcz-vLCDyTs",
-  authDomain: "boxerapp-beb5c.firebaseapp.com",
-  projectId: "boxerapp-beb5c",
-  storageBucket: "boxerapp-beb5c.appspot.com",
-  messagingSenderId: "978376103925",
-  appId: "1:978376103925:web:d21b53861e25f54bd632bc",
-  measurementId: "G-N91VM3FH78",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const auth = initializeAuth(FIREBASE_APP, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 export const db = getFirestore(FIREBASE_APP);
 // export const functions = getFunctions(FIREBASE_APP);
